9aeeaf8c9b46b7380acff7e30d76a749be58a5fd
Mardini <mardini1414@gmail.com>
2025-01-28 16:13:56 +0700

    fix: typo component droppable and update version to 0.0.4

--- src/components/Droppable.tsx
+++ src/components/Droppable.tsx
@@ -1,6 +1,6 @@
 import { TDroppableProps } from '../types';
 
-export default function Dropable({ children, className, shelfKey }: TDroppableProps) {
+export default function Droppable({ children, className, shelfKey }: TDroppableProps) {
   return (
     <div shelf-data="droppable" shelf-data-key={shelfKey} className={className}>
       {children}
